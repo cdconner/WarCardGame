a325f55913c8d665898c00d60c61b7c0b8d62532
Christopher Conner <chris.conner@gmail.com>
2020-06-27 15:43:00 -0500

    War Card Game

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -8,10 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var randNum1 = 2
+    @State private var randNum2 = 2
+    
+    @State private var score1 = 0
+    @State private var score2 = 0
+    
     var body: some View {
-        Text("Hello, world!").padding()
+       
+        ZStack {
+            Image("Background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                
+                Spacer()
+                
+                HStack {
+                    Image("card" + String(randNum1))
+                    Image("card" + String(randNum2))
+                }
+                Spacer()
+                Button(action: {
+                    //Randomize the state properties
+                    self.randNum1 = Int.random(in: 2...14)
+                    self.randNum2 = Int.random(in: 2...14)
+                    
+                    //Update Scores
+                    if self.randNum1 > self.randNum2 {
+                        score1 += 1
+                    } else if self.randNum2 > self.randNum1 {
+                        score2 += 1
+                    }
+                    
+                    
+                }, label: {
+                    Image("dealbutton")
+                        .renderingMode(.original)
+                })
+                Spacer()
+                
+                HStack {
+                    VStack {
+                        Text("Player")
+                            .bold()
+                            .padding(.bottom, 20)
+                        Text(String(score1))
+                    }
+                    .padding(.leading, 20)
+                    .foregroundColor(.white)
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .bold()
+                            .padding(.bottom, 20)
+                        Text(String(score2))
+                    }
+                    .padding(.trailing, 20)
+                    .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

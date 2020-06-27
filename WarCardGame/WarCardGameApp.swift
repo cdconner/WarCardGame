fcb3a3be8a001315be7e67c7b52eac53dac5105f
Christopher Conner <chris.conner@gmail.com>
2020-06-27 14:54:03 -0500

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Christopher Conner on 6/27/20.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
